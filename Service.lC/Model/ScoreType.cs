efb549bc179b97cab1f67ab68ab2c8a6c9d59f2c
KloderGit <idzhyan.develop@outlook.com>
2021-04-15 10:30:54 +0300

    add ScoreType

---
+++ Service.lC/Model/ScoreType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.lC.Model
+{
+    public class ScoreType : Base
+    {
+        public int MinRate { get; set; }
+        public int MaxRate { get; set; }
+
+        public string Rate { get; set; }
+    }
+}
