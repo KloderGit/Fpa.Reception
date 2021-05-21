902f7e9e571e3288372fdd81839bed35cc125bbb
KloderGit <idzhyan.develop@outlook.com>
2021-05-21 14:31:11 +0300

    add Teacher Schedule

---
+++ Fpa.Reception/Controllers/Settings/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace reception.fitnesspro.ru.Controllers.Settings
+{
+    public class SettingsController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
